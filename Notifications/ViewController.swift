9d3c5b28e37aec3cc650aeef290e8a395448f4ea
DSadowski3 <dsadowski2001@dominiks-macbook-pro.local>
2017-03-03 12:03:35 -0600

    Initial Commit

---
+++ Notifications/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Notifications
+//
+//  Created by Dominik Sadowski on 3/3/17.
+//  Copyright © 2017 Dominik Sadowski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
