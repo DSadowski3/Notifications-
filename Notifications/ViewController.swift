0898e3dd45c37387d9448304826808a72993823e
DSadowski3 <dsadowski2001@Dominiks-MacBook-Pro.local>
2017-03-03 13:50:08 -0600

    sending local notifications to users

--- Notifications/ViewController.swift
+++ Notifications/ViewController.swift
@@ -7,17 +7,57 @@
 //
 
 import UIKit
+import UserNotifications
+import UserNotificationsUI
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { (granted, error) in
+            
+            if granted {
+                print("Notification access granted")
+            } else {
+                print(error?.localizedDescription ?? "")
+            }
+        }
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func notifyButtonTapped(sender: UIButton) {
+        scheduleNotification(inSeconds: 5) { (success) in
+            if success {
+                print("Successfully scheduled Notification")
+            } else {
+                print("Error in notificaiton")
+            }
+        }
+    }
+    
+    func scheduleNotification(inSeconds: TimeInterval, completion: @escaping (_ Success: Bool) -> ()) {
+        
+        
+        let notificaiton = UNMutableNotificationContent()
+        notificaiton.categoryIdentifier = "myNotificationCategory"
+        notificaiton.title = "New Notificaiton"
+        notificaiton.subtitle = "I Love to notify"
+        notificaiton.body = "The new notificaiton will let you know more about yourself as a user!"
+        
+        let notificationTrigger = UNTimeIntervalNotificationTrigger(timeInterval: inSeconds, repeats: false)
+        
+        let request = UNNotificationRequest(identifier: "myNotif", content: notificaiton, trigger: notificationTrigger)
+        
+        UNUserNotificationCenter.current().add(request) { (error) in
+            
+            if error != nil {
+                print(error ?? "")
+                completion(false)
+            }else {
+                completion(true)
+            }
+        }
     }
 
 
